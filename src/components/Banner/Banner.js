2a2b83dd73ddc1404cc6595c382f4ebcd8e922fb
Lucas Nobre <lucasnobreoficial@hotmail.com>
2023-11-03 09:47:36 -0300

    Remoção de arquivos

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,20 @@
+import styled from "styled-components"
+
+const HeaderComponent = styled.header`
+    background-color:#6278f7;
+    text-align:center;
+`
+
+const ImageComponent = styled.img`
+    max-width:100%;
+`
+
 function Banner() {
     return (
-        <img src="/imgs/banner.png" alt="Banner prnicipal da página do Organo"/>
+        <HeaderComponent>
+            <ImageComponent src="/imgs/banner.png"
+            alt="Banner prnicipal da página do Organo"/>
+        </HeaderComponent>
     )
 }
 
